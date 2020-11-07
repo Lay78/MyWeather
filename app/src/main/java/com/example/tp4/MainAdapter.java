55f9774310baaffa9ab36cbb4734267e5d5c651a
Lay78 <antoine_lay@yahoo.fr>
2020-11-07 19:31:32 +0100

    MainAdapter implemented

--- app/src/main/java/com/example/tp4/MainAdapter.java
+++ app/src/main/java/com/example/tp4/MainAdapter.java
@@ -1,7 +1,9 @@
 package com.example.tp4;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -15,25 +17,32 @@ public class MainAdapter extends RecyclerView.Adapter<MainAdapter.ViewHolder> {
         cities_list = cities;
     }
 
+    //ViewHolder = visually represent an item of the ArrayList in the RecyclerView
     @NonNull
     @Override
     public MainAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.row, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MainAdapter.ViewHolder holder, int position) {
-
+        holder.city_name.setText(cities_list.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return cities_list.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView city_name;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            city_name = (TextView) itemView.findViewById(R.id.city);
         }
     }
 }
