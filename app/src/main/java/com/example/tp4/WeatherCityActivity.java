056d187b86db26410b2619e7839ee2c589648fd5
Lay78 <antoine_lay@yahoo.fr>
2020-11-13 18:57:14 +0100

    add ActionBar to WeatherCityActivity

--- app/src/main/java/com/example/tp4/WeatherCityActivity.java
+++ app/src/main/java/com/example/tp4/WeatherCityActivity.java
@@ -1,9 +1,12 @@
 package com.example.tp4;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class WeatherCityActivity extends AppCompatActivity {
@@ -14,6 +17,8 @@ public class WeatherCityActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather_city);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         tv_city = findViewById(R.id.tv_city);
 
@@ -23,5 +28,27 @@ public class WeatherCityActivity extends AppCompatActivity {
             CityModel cityModel = (CityModel) intent.getSerializableExtra("data");
             tv_city.setText(cityModel.getCity_name());
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_delete) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
